10fbb33d33f88e1b3d54e710a8593e5f44af87a8
Andrew Cole <andrew.cole@illallangi.com>
2013-05-22 12:52:17 +1000

    Initial Commit

---
+++ Illallangi.StringExtensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Illallangi Enterprises">Copyright © 2012 Illallangi Enterprises</copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Illallangi String Extensions")]
+[assembly: AssemblyDescription("Illallangi String Extensions")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Illallangi Enterprises")]
+[assembly: AssemblyProduct("Illallangi String Extensions")]
+[assembly: AssemblyCopyright("Copyright © Illallangi Enterprises 2013")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6aef96f0-1092-4727-b822-b8910d23e1c4")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Illallangi.StringExtensions/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Illallangi
+{
+    public static class StringExtensions
+    {
+        public static string ToMd5Hash(this string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            var sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        public static string CreatePath(this string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            return path;
+        }
+    }
+}
